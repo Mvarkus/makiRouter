3079e8e3fddc9580b2e9073330319a8193557538
Maksim Varkus <mvarkus.w@gmail.com>
2020-11-24 19:10:01 +0000

    Remove examples

--- public/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require __DIR__ . "/../vendor/autoload.php";
-
-Mvarkus\MakiRouter::init('../routes/web.php');
-Mvarkus\MakiRouter::routeRequest(
-    Symfony\Component\HttpFoundation\Request::createFromGlobals()
-)->send();
